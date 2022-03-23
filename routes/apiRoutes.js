6fa6292df72063fb6cc23e5def38b9ebe28c9642
Gus <gustavo.canales@me.com>
2022-03-23 22:59:24 +0100

    added console log

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,7 +23,7 @@ module.exports = function (app) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed    
-    console.log(req.user)  
+    //console.log(req.user)  
 
     if (req.user.role === "produccion" || req.user.role === "admin") {
       res.cookie("usuario",req.user.email)
