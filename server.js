dd2d9336fc22156cc99436d3fc8fbce0b2705923
Gus <gustavo.canales@me.com>
2022-03-25 00:30:58 +0100

    added CORS

--- server.js
+++ server.js
@@ -128,6 +128,7 @@ app.use(function(req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept, x-token,"
   );
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 // END CORS
